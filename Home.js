681e47413b7626f0efef7b83633dd3d1dcf8c830
odo149 <92023309+odo149@users.noreply.github.com>
2021-10-27 16:02:32 +0000

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Image, Text, View } from 'react-native';
 
 export default function Home({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Welcome sneakerhead!</Text>
+        <Text style={{fontSize:30}}>Welcome sneakerhead!</Text>
+        <View>
+          <Image
+            fadeDuration={1000} 
+            style={{width:370,height:100}}
+            source={require('./assets/ADISLOGO.png')}/>
+        </View >
+      <View>
+        <Image 
+            fadeDuration={1000} 
+            style={{width:370,height:100,}}
+            source={require('./assets/JORDAN.png')}/>
+      </View>
+        <Image 
+            fadeDuration={1000} 
+            style={{width:370,height:100}}
+            source={require('./assets/CONVERSE.png')}/>
+      <View>
+      </View>
+        <Image 
+            fadeDuration={1000} 
+        style={{width:370,height:100}}
+        source={require('./assets/NIKELOGO.png')}/>
+      <View>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
--- Login.js
+++ Login.js
@@ -1,11 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput,Button,View,TouchableOpacity, ImageBackground } from 'react-native';
 
-export default function Login() {
+export default function Login({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Hello sneakerhead!</Text>
+
+      <ImageBackground 
+      blurRadius={30}
+      justifyContent={'space-evenly'}
+      source={require("./assets/CONVERSE.png")}>
+
+        <Text style={{
+          fontSize: 40,
+          color: 'black'
+          }}>Hello sneakerhead!
+        </Text>
+        <Text style = {{
+          fontSize: 25,
+          color: 'black'
+          }}>username or email
+        </Text>
+        <View style= {{
+        backgroundColor: '#e8e9eb',
+        borderRadius: 20,
+        padding: 20,
+        width: 370
+      }}>
+        <TextInput 
+          placeholder= "username or email"
+        />
+      </View>
+      <Text style = {{
+        fontSize: 25,
+        color: 'black'
+      }}>password</Text>
+      <View style= {{
+        backgroundColor: '#e8e9eb',
+        borderRadius: 20,
+        padding:20,
+        width: 370
+      }}>
+        <TextInput 
+        placeholder = 'password'
+        secureTextEntry= {true}/>
+      </View >
+      </ImageBackground>
+      <View style = {{paddingTop: 50,
+      width: 370,
+      elevation: 300}}>
+        <Button
+            onPress={() => navigation.navigate('Home')}
+            title = "SIGN IN"
+            color = "#241f1f"/>
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +64,7 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
